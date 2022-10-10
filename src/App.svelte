847e7dd76a40345f4fb455e0fa2b742730a3f001
Valentin <93974733+valentindush@users.noreply.github.com>
2022-10-10 08:52:27 +0200

    ing

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   
 </script>
 
-<main>
+<main class="">
 </main>
 
 <style>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+<nav class="w-screen">
+    <div class="">
+        <div class="social">
+
+        </div>
+    </div>
+    <div class="">
+        <div class="logo">
+            
+        </div>
+        <div class="nav-links"></div>
+        <div class="btn"></div>
+    </div>
+</nav>
