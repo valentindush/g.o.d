8a21657c8d02f0763a43a8977210e79fd9069343
Valentin <93974733+valentindush@users.noreply.github.com>
2022-10-13 21:15:51 +0200

    abt

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -2,5 +2,14 @@
 
 </script>
 <main class="">
+    <header class="title mt-7 text-center md:texts">
+        <h2 class="text-orange-500 text-2xl font-semibold">GIRLS FOF DESTINY</h2>
+        <p class="text-gray-300 text-sm mt-4">"Move the paper out and they read"</p>
+    </header>
 
+    <div class="text-center px-[8rem] md:px-1">
+        <p class="text-gray-300 text-xl mt-12 opacity-70 ">
+            Girls Of Destiny is a family that grew up in the mind of a boy due to the trauma of not having a sibling to the point where he was put out and people saw it. It always delights my heart when I teach girls. Girls Of Destiny We help our newborn children We always help their children to live a healthy life, we advise others to go back to school, we help the needy and make them support projects, we also visit schools and set up clubs and they sit on the appointed day. the other gives them a poem and the other gives them a testimony, which when they do makes them bold. 
+        </p>
+    </div>
 </main>
