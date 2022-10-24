004b7ad1f835d175e87d1dcadbdab2fc406ae784
Valentin <93974733+valentindush@users.noreply.github.com>
2022-10-24 19:19:01 +0200

    fx

---
+++ src/pages/Contact.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  
+  
+  </script>
+  <main class="">
+      <header class="title mt-7 text-center md:texts">
+          <h2 class="text-orange-500 text-2xl font-semibold">CONTACT US</h2>
+          <p class="text-gray-300 text-sm mt-4">"Move the paper out and they read"</p>
+      </header>
+  
+      <div class="text-center px-[8rem] md:px-1">
+        <p class="text-gray-300 text-xl mt-12 opacity-70 ">
+            We are always available to hear from your advices, We will respond to your feedback,
+            also feel free to to talk to us for support of sponsorship.
+            once we see it. DROP US A LINE.  
+            <p>
+      </div>
+
+      <div class="form w-full h-fit p-4 bg-gray-100 rounded-lg mt-12">
+        <form action="#">
+            <input type="text" placeholder="Names" class="block">
+            <input type="email" placeholder="Your email address" class="block">
+            <textarea placeholder="Message" class="block"></textarea>
+        </form>
+      </div>
+         
+  </main>
