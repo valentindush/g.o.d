f731b4d98e06f2d2c0d2b6ab07a650865b57eff8
Valentin <93974733+valentindush@users.noreply.github.com>
2022-10-24 21:05:35 +0200

    finished donate

---
+++ src/pages/Donate.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  
+  
+</script>
+<main class="">
+    <header class="title mt-7 text-center md:texts">
+        <h2 class="text-orange-500 text-2xl font-semibold">GIVE A DONATION</h2>
+        <p class="text-gray-300 text-sm mt-4">"Move the paper out and they read"</p>
+    </header>
+
+    <div class="text-center px-[8rem] md:px-1">
+        <p class="text-gray-300 text-xl mt-12 opacity-70 ">
+            Do this for young children who needs help. for young girls and adolscents, your participation will make this even bigger and help more people than you think. 
+        </p>
+    </div>
+    <div class="bg-gray-100 p-4 rounded-lg mt-4">
+        <div class="flex justify-center gap-3 flex-wrap">
+            <div class="border border-yellow-400 rounded-lg w-full h-[300px] flex flex-col items-center justify-center gap-8 text-center md:w-[250px]">
+                <p class="text-yellow-400">Use MTN mobile money</p>
+                <p>Use this code :
+                    *182*1*0 788 415 849*AMOUNT*PIN#</p>
+            </div>
+            <div class="border border-green-400 rounded-lg w-full h-[300px] flex flex-col items-center justify-center gap-8 text-center md:w-[250px]">
+                <p class="text-green-400">Use this bank account</p>
+                <p>Account number: 7899 24952 992529 24529</p>
+            </div>
+           
+        </div>
+        <div class="flex justify-center gap-3 mt-3 flex-wrap">
+            <div class="border border-blue-400 rounded-lg w-full h-[300px] flex flex-col items-center justify-center gap-8 text-center md:w-[250px]">
+                <p class="text-blue-400">Use paypal</p>
+                <p>Send to user: G.O.D</p>
+            </div>
+            <div class="border border-orange-400 rounded-lg w-full h-[300px] flex flex-col items-center justify-center gap-8 text-center md:w-[250px]">
+                <p class="text-orange-400">Or for more info </p>
+                <p>Contact us on +250 788 415 849</p>
+            </div>
+        </div>  
+    </div>
+</main>
