8a8b30eac8356b2fdcaea84a846434008ce247a9
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-08 09:07:12 +0200

    added swiperjs

--- src/lib/carousel.svelte
+++ src/lib/carousel.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import FaSolidAngleLeft from "svelte-icons-pack/fa/FaSolidAngleLeft";
     import Icon from "svelte-icons-pack";
+    import 'swiper/css/navigation';
+    import 'swiper/css/pagination';
+    import 'swiper/css/scrollbar';
+    import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
     const images:string[] = [
         'image1.png',
         'image2.png',
@@ -26,21 +30,28 @@
             behavior: 'smooth'
         })
     }
+
+    import {Swiper, SwiperSlide} from 'swiper/svelte';
+    import 'swiper/css'
+
 </script>
 <div class="px-24 mt-4 py-6 relative md:px-1">
     <h2 class="text-2xl text-gray-700 font-semibold py-4 md:text-center">Take a look at some of our beautiful images</h2>   
-    <div class="px-0 md:px-7">
-        <button on:click={scrollLeft} class="bg-gray-200 absolute w-12 h-12 flex items-center justify-center top-[40%] left-5 cursor-pointer active:scale-[.98] md:left-0 md:top-1/2 md:scale-75">
-            <Icon src={FaSolidAngleLeft} color="gray" size="24" />
-        </button>
-        <button on:click={scrollRight} class="bg-gray-200 absolute w-12 h-12 flex items-center justify-center top-[40%] right-5 cursor-pointer active:scale-[.98] md:right-0 md:top-1/2 md:scale-75">
-            <Icon src={FaSolidAngleLeft} color="gray" size="24" className='rotate-180' />
-        </button>
-        <div bind:this={div} class="flex gap-2 w-full overflow-hidden relative px-1">
-            
-            {#each images as img}
-                <img class="w-[20rem] h-[14rem] object-cover" src={`./img/${img}`} alt="mover the paper out and they read">
-            {/each}
-        </div>
-    </div>
+ 
+    <Swiper
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    spaceBetween={20}
+    slidesPerView={3}
+    navigation
+    pagination={{ clickable: true }}
+    on:slideChange={() => console.log('slide change')}
+    on:swiper={(e) => console.log(e.detail[0])}
+  >
+
+  {#each images as image}
+    <SwiperSlide>
+      <img src={`./img/${image}`} alt="move the paper out and they read" class="w-full h-64 object-cover rounded-lg" />
+    </SwiperSlide>
+    {/each}
+  </Swiper>
 </div>
